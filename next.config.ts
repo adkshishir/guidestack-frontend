1b837d761f5c04a80ed039d59caccb2ef4843fee
adkshishir <adhikarishishir50@gmail.com>
2026-03-27 16:23:08 +0545

    fix(config): correct localhost port for image uploads

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '4007',
+        port: '3000',
         pathname: '/uploads/**',
       },
       {
